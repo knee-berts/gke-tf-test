d8e601580f9aae044482a74cabffaa67bb8f6e33
Nick Eberts <nickeberts@google.com>
2022-10-14 10:31:18 -0400

    added control

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 ## Initial envars
-export PROJECT_PREFIX=
-export FOLDER_ID=
-export BILLING_ID=
+export PROJECT_PREFIX=ne
+export FOLDER_ID="115792097683"
+export BILLING_ID="0178B5-7A3501-25922A" 
 
 ## Added from an experiment
 
